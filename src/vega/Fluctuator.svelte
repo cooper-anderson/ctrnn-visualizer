cfe474721bbfafaa343e98c6648ffc7b096d00cc
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-07-25 19:02:01 -0700

    Add vega spec for basic plot for fluctuator testing

---
+++ src/vega/Fluctuator.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { onDestroy } from 'svelte';
+	import { Fluctuator } from 'ctrnn.js';
+
+	import { Vega } from 'svelte-vega';
+	import spec from '../spec/line';
+
+	type Datum = { x: number; y: number; c: number };
+	let table: Datum[] = [];
+	$: data = { table };
+	let t = 0;
+
+	const flux = new Fluctuator(2);
+	flux.convergence_rate *= 5;
+	// // flux.amplitude /= 10;
+	// let data = getData(flux);
+
+	// function getData(flux: Fluctuator): { table: Datum[] } {
+	// 	const table: Datum[] = [];
+
+	table.push(
+		...[
+			{ c: 0, x: 0, y: flux.center },
+			{ c: 1, x: 0, y: flux.value },
+			{ c: 2, x: 0, y: flux.amplitude },
+			{ c: 3, x: 0, y: 0 }
+		]
+	);
+
+	let last = flux.value;
+	let interval = setInterval(() => {
+		const reward = (Math.abs(last) - Math.abs(flux.value)) / 10;
+		last = flux.value;
+		flux.update(0.05, reward);
+		const time = Math.round(t * 100) / 100;
+
+		const next = [
+			{ c: 0, x: time, y: flux.center },
+			{ c: 1, x: time, y: flux.value },
+			{ c: 2, x: time, y: flux.amplitude },
+			{ c: 3, x: time, y: reward * 100 }
+		];
+		table = [...table, ...next];
+		// if (table.length < 400) table = [...table, ...next];
+		// else table = [...table.slice(4), ...next];
+
+		t += 0.05;
+	}, 5);
+
+	onDestroy(() => {
+		clearInterval(interval);
+	});
+</script>
+
+<Vega {data} {spec} view={undefined} />
